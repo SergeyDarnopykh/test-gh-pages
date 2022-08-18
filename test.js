b05373707843a9c128a0a7fa967063868a878af0
Sergey Proverov <sergey.proverov@gmail.com>
2022-08-18 21:33:12 +0300

    Add test file

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log('First log');
+
+setTimeout(() => {
+    console.log('set timeout 10')
+}, 10);
+
+setTimeout(() => {
+    console.log('set timeout 0')
+}, 0);
+
+console.log('Last log');
+
